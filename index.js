d42993056e17b0223434ea6c9d10eafdf471d229
GosGko <bosspetkov@abv.bg>
2023-05-19 18:43:42 +0300

    rpe

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ for (var i = 0 ; i < drumsLength; i++) {
 document.querySelectorAll(".drum")[i].addEventListener("click" , buttonClicked);
 
 function buttonClicked() {
-    var audio = new Audio('')
+
+    this.style.color = "white";
+    
+
+    //var audio = new Audio('sounds/tom-1.mp3');
+    //audio.play();
 }
 }
