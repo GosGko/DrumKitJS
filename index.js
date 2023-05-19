0e2f8983188ed663263ec740c8af62cf35ecd32c
GosGko <bosspetkov@abv.bg>
2023-05-19 14:57:41 +0300

    Give the info

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var drumsLength = document.querySelectorAll(".drum").length;
+
+for (var i = 0 ; i < drumsLength; i++) {
+
+
+document.querySelectorAll(".drum")[i].addEventListener("click" , buttonClicked);
+
+function buttonClicked() {
+    var audio = new Audio('')
+}
+}
